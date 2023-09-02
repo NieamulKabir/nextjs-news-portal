91a604c9d971e3f68fec0eed82bea55bb43c7695
NieamulKabir <kabir000079@gmail.com>
2023-09-02 23:13:57 +0600

    json server setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,7 @@ import Head from "next/head";
 import RootLayout from "@/components/Layouts/RootLayout";
 import Banner from "@/components/UI/Banner";
 
-
-const HomePage = () => {
+const HomePage = ({allNews}) => {
   return (
     <>
       <Head>
@@ -23,4 +22,15 @@ export default HomePage;
 
 HomePage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:5000/news");
+  const data = await res.json();
+
+  return {
+    props: {
+      allNews: data,
+    },
+  };
 };
