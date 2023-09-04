602ca1133cd2a59f67db5b494db13513a7185c44
NieamulKabir <kabir000079@gmail.com>
2023-09-05 01:10:48 +0600

    getstaticpath

--- src/pages/news/[newsId].js
+++ src/pages/news/[newsId].js
@@ -1,10 +1,74 @@
 import React from "react";
+import { Col, Row } from "antd";
+import {
+  UserOutlined,
+  CalendarOutlined,
+  CommentOutlined,
+  ProfileOutlined,
+} from "@ant-design/icons";
+import Image from "next/image";
 import RootLayout from "../../components/Layouts/RootLayout";
 
 const NewsDetailPage = ({ news }) => {
   return (
     <div>
-      <h1>{news?.title}</h1>
+      <Row style={{ marginTop: "80px", alignItems: "center" }}>
+    <Col md={6} lg={12}>
+      <Image
+        alt="example"
+        src={news?.image_url}
+        width={500}
+        height={300}
+        responsive
+      />
+    </Col>
+    <Col md={6} lg={12} style={{ paddingLeft: "20px" }}>
+      <h1 style={{ fontSize: "30px" }}>{news?.title}</h1>
+      <span
+        style={{
+          color: "gray",
+          display: "block",
+          fontSize: "20px",
+        }}
+      >
+        <UserOutlined /> {news?.author}
+      </span>
+      <div
+        className="line"
+        style={{
+          height: "5px",
+          margin: "20px 0",
+          background: "#000",
+          width: "100%",
+        }}
+      ></div>
+
+      <p
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          width: "100%",
+          color: "gray",
+          margin: "10px 0px",
+          fontSize: "20px",
+        }}
+      >
+        <span>
+          <CalendarOutlined /> {news?.release_date}
+        </span>
+        <span>
+          <CommentOutlined /> {news?.comment_count} Comments
+        </span>
+        <span>
+          <ProfileOutlined /> {news?.category}
+        </span>
+      </p>
+
+      <p style={{ fontSize: "25px", fontWeight: "lighter" }}>
+        {news?.description}
+      </p>
+    </Col>
+  </Row>
     </div>
   );
 };
